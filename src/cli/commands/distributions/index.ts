83a47003871387ef4a14a3021d2820133a9c80d4
Tim Pechersky <tim.pechersky@gmail.com>
2025-03-19 18:21:02 +0800

    added remove distribution cli method

--- src/cli/commands/distributions/index.ts
+++ src/cli/commands/distributions/index.ts
@@ -2,8 +2,10 @@ import { Command } from "commander";
 import { listCommand } from "./list";
 import { addCommand } from "./add";
 import { stateCommand } from "./state";
+import { removeCommand } from "./remove";
 export const distributionsCommand = new Command("distributions")
   .description("Manage distributions")
   .addCommand(listCommand)
   .addCommand(addCommand)
-  .addCommand(stateCommand);
+  .addCommand(stateCommand)
+  .addCommand(removeCommand);
